bb31a61cc24bce93a5ba8afb317e3537ba00d7c1
Haidar Shalahuddin <61963873+haidar280796@users.noreply.github.com>
2025-10-25 09:31:08 +0700

    Update Supabase configuration
    Updated Supabase URL and cleared the anon key.

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tpopirqfvurycnuqhcgi.supabase.co' // Ganti dengan URL kamu
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwb3BpcnFmdnVyeWNudXFoY2dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxNjU1MzYsImV4cCI6MjA2Mzc0MTUzNn0.wqk0elCCDtZFaiVcpi6QU67F34gxhkzJn8toilP4_Cs' // Ganti dengan anon key kamu
+const supabaseUrl = 'https://xxx.supabase.co' // Ganti dengan URL kamu
+const supabaseKey = '' // Ganti dengan anon key kamu
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
